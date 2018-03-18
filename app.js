4ea92c487e2f2c0a53ede84b484f953108f5f35d
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-03-18 14:32:40 -0400

    working

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello Friend');
+var fs = require('fs');
+var readline = require('readline');
+var stream = require('stream');
+
+var instream = fs.createReadStream('voter/orange.txt');
+var outstream = new stream;
+var rl = readline.createInterface(instream, outstream);
+rl.on('line', function(line){
+  var results = line.toUpperCase();
+  if(results.includes('@')){
+    console.log(line);
+  }
+}).on('close', function(){
+  console.log('All Done!');
+});
