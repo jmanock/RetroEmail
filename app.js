e60d03cd8f4451d62793e497c2700e4c403f7382
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-03-18 14:42:22 -0400

    easier to read

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
 
-var instream = fs.createReadStream('voter/orange.txt');
+var instream = fs.createReadStream('voter/seminole.txt');
+// var instream = fs.createReadStream('voter/orange.txt');
 var outstream = new stream;
 var rl = readline.createInterface(instream, outstream);
 rl.on('line', function(line){
   var results = line.toUpperCase();
-  if(results.includes('@')){
-    console.log(line);
+  if(results.includes('') && results.includes('')){
+    results = results.split(/[\t]+/);
+    console.log(results);
   }
 }).on('close', function(){
   console.log('All Done!');
