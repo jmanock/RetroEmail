126f1a5544ec8b5ca95a687beca1f398bef4c88e
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-03-19 04:37:51 -0400

    working on the email list

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ var instream = fs.createReadStream('voter/seminole.txt');
 // var instream = fs.createReadStream('voter/orange.txt');
 var outstream = new stream;
 var rl = readline.createInterface(instream, outstream);
+var count = 0;
 rl.on('line', function(line){
-  var results = line.toUpperCase();
-  if(results.includes('') && results.includes('')){
+  // var results = line.toUpperCase();
+  var results = line;
+
+  // if(results.includes('') && results.includes('')){
+  if(results.includes('@')){
+    count = count + 1;
     results = results.split(/[\t]+/);
+
     for(var i = 0; i<results.length; i++){
-      console.log(results[i]);
+       if(results[i].includes('@')){
+         console.log(results[i]);
+       }
     }
   }
 }).on('close', function(){
+// console.log(count);
   console.log('All Done!');
 });
