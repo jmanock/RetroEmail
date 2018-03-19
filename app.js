e2ae0cff8403ea71ca99c8af7130a670b08d3392
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-03-19 12:02:14 -0400

    city check

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ var readline = require('readline');
 var stream = require('stream');
 
 var instream = fs.createReadStream('voter/seminole.txt');
-// var instream = fs.createReadStream('voter/orange.txt');
+//var instream = fs.createReadStream('voter/orange.txt');
 var outstream = new stream;
 var rl = readline.createInterface(instream, outstream);
 var count = 0;
 rl.on('line', function(line){
-  // var results = line.toUpperCase();
-  var results = line;
-
+   var results = line.toUpperCase();
+  // var results = line;
+  //count = count + 1;
   // if(results.includes('') && results.includes('')){
   if(results.includes('@')){
-    count = count + 1;
+
     results = results.split(/[\t]+/);
 
     for(var i = 0; i<results.length; i++){
-       if(results[i].includes('@')){
-         console.log(results[i]);
-       }
+      if(results[i].includes('FERN PARK')){
+        count = count + 1;
+      }
     }
   }
 }).on('close', function(){
-// console.log(count);
+  console.log(count);
   console.log('All Done!');
 });
