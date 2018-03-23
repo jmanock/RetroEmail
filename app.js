d9e644ce791a5ccb439b3376a26d0d71331a79ba
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-03-23 15:25:10 -0400

    added more emails to the list

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
 
-var instream = fs.createReadStream('voter/seminole.txt');
-//var instream = fs.createReadStream('voter/orange.txt');
+//var instream = fs.createReadStream('voter/seminole.txt');
+var instream = fs.createReadStream('voter/orange.txt');
 var outstream = new stream;
 var rl = readline.createInterface(instream, outstream);
 var count = 0;
@@ -12,33 +12,52 @@ rl.on('line', function(line){
   // var results = line;
   //count = count + 1;
   // if(results.includes('') && results.includes('')){
-  if(results.includes('FERN PARK')){
-    //var lastName, firstName, middleName;
-    results = results.split(/[\t]+/);
-    count = count + 1;
-    for(var i = 0; i<results.length; i++){
-      var lastName = results[2];
-      var firstName = results[3];
-      var middleName = results[4];
-      //console.log(results[i]);
-      var test = results[10];
 
-      if(results[i] === 'FERN PARK'){
-        var address = results[i-2];
-        var address2 = results[i-1];
+  // if(results.includes('FERN PARK')){
+  //   //var lastName, firstName, middleName;
+  //   results = results.split(/[\t]+/);
+  //   count = count + 1;
+  //   for(var i = 0; i<results.length; i++){
+  //     var lastName = results[2];
+  //     var firstName = results[3];
+  //     var middleName = results[4];
+  //     //console.log(results[i]);
+  //     var test = results[10];
+  //
+  //     if(results[i] === 'FERN PARK'){
+  //       var address = results[i-2];
+  //       var address2 = results[i-1];
+  //     }
+  //   }
+  //    if(count >= 300 && count < 400){
+  //      console.log(count, firstName, lastName, address, address2, test);
+  //   }
+  //
+  //   // if(count === 296){
+  //   //   console.log(results);
+  //   // }
+  //
+  //   //console.log(count, firstName, lastName, address, address2);
+  //
+  // }
+
+  var email;
+  if(results.includes('OCOEE')){
+    results = results.split(/[\t]+/);
+    for(var i = 0; i<results.length; i++){
+        if(results[i].includes('@')){
+          // console.log(results[i],i);
+          count = count +1;
+          email = results[i];
+        }
+    }
+    if(count >= 999){
+      if(email !== undefined){
+        console.log(email)
       }
     }
-     if(count >= 300 && count < 400){
-       console.log(count, firstName, lastName, address, address2, test);
-    }
-
-    // if(count === 296){
-    //   console.log(results);
-    // }
-
-    //console.log(count, firstName, lastName, address, address2);
   }
-}).on('close', function(){
 
+}).on('close', function(){
   console.log('All Done!');
 });
